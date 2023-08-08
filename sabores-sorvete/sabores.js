668e042d248b05d9e803aff66cf94d2d6ea6398f
Morenofael <91558374+Morenofael@users.noreply.github.com>
2023-08-08 17:43:49 -0300

    Add files via upload

---
+++ sabores-sorvete/sabores.js
@@ -0,0 +1,10 @@
+let inputSabores = document.getElementById("sabor-sorvete");
+let mensagem = document.getElementById("mensagem");
+
+function verificaSabor() {
+    if(inputSabores.value.length > 3){
+        if(inputSabores.value.to == "chocolate")
+    }
+}
+
+inputSabores.addEventListener("input", () => verificaSabor());
